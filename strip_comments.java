39d53bf5915ed565fbf98ef61cec01f10e2e0442
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-03-18 16:50:53 +0100

    Create strip_comments.java

---
+++ strip_comments.java
@@ -0,0 +1,31 @@
+public class StripComments {
+
+    public static String stripComments(String input, String[] markers) {
+        String[] lines = input.split("\n");
+        for (int i = 0; i < lines.length; i++) {
+            String line = lines[i];
+            for (String marker : markers) {
+                int commentIndex = line.indexOf(marker);
+                if (commentIndex != -1) {
+                    line = line.substring(0, commentIndex);
+                }
+            }
+            line = line.replaceAll("\\s+$", "");
+            lines[i] = line;
+        }
+
+        StringBuilder outputBuilder = new StringBuilder();
+        for (int i = 0; i < lines.length; i++) {
+            String line = lines[i];
+            if (line.trim().length() > 0 || i == lines.length - 1) {
+                outputBuilder.append(line);
+                if (i < lines.length - 1) {
+                    outputBuilder.append("\n");
+                }
+            }
+        }
+
+        String output = outputBuilder.toString();
+        return output;
+    }
+}
