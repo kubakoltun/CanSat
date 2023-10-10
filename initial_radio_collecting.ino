84f3b6763726eeba52974a0c4a986960f0b403e1
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-10-10 19:36:12 +0200

    Create initial_radio_collecting.ino

---
+++ initial_radio_collecting.ino
@@ -0,0 +1,37 @@
+#include <SPI.h>
+#include <LoRaWan-Arduino.h>
+
+#define SENDER
+// 868MHz
+#define BAND 868E6  
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  LoRaWAN.begin(BAND);
+
+#ifdef SENDER
+  // Wysyłanie danych
+  LoRaWAN.send("Hello, World!", 13);
+#else
+  // Odbieranie danych
+  LoRaWAN.onReceive([](const uint8_t *data, size_t size) {
+    Serial.print("Odebrano: ");
+    Serial.write(data, size);
+    Serial.println();
+  });
+  LoRaWAN.receive();
+#endif
+}
+
+void loop() {
+#ifdef SENDER
+  // Wysyłanie danych co 10 sekund
+  LoRaWAN.loop();
+  delay(10000);
+#else
+  // Odbieranie danych
+  LoRaWAN.loop();
+#endif
+}
