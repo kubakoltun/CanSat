91da8ad108dddef4b330ee1cc61bd941d4b351af
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-10-12 19:33:49 +0200

    Update initial_radio_collecting.ino

--- initial_radio_collecting.ino
+++ initial_radio_collecting.ino
@@ -11,27 +11,27 @@ void setup() {
 
   LoRaWAN.begin(BAND);
 
-#ifdef SENDER
-  // Wysyłanie danych
-  LoRaWAN.send("Hello, World!", 13);
-#else
-  // Odbieranie danych
-  LoRaWAN.onReceive([](const uint8_t *data, size_t size) {
-    Serial.print("Odebrano: ");
-    Serial.write(data, size);
-    Serial.println();
-  });
-  LoRaWAN.receive();
-#endif
+  #ifdef SENDER
+    // Wysyłanie danych
+    LoRaWAN.send("Hello, World!", 13);
+  #else
+    // Odbieranie danych
+    LoRaWAN.onReceive([](const uint8_t *data, size_t size) {
+      Serial.print("Odebrano: ");
+      Serial.write(data, size);
+      Serial.println();
+    });
+    LoRaWAN.receive();
+  #endif
 }
 
 void loop() {
-#ifdef SENDER
-  // Wysyłanie danych co 10 sekund
-  LoRaWAN.loop();
-  delay(10000);
-#else
-  // Odbieranie danych
-  LoRaWAN.loop();
-#endif
+  #ifdef SENDER
+    // Wysyłanie danych co 10 sekund
+    LoRaWAN.loop();
+    delay(10000);
+  #else
+    // Odbieranie danych
+    LoRaWAN.loop();
+  #endif
 }
